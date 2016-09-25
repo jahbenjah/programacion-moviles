ac89e53563a6af5f05c9abc33605ffd79f96786c
BenjaminCamacho <BenjaminCamacho@DESKTOP-POLQCT7>
2016-09-24 20:41:16 -0700

    se implementa la fucncionalidad básica requerida.

---
+++ Actividad4/Actividad4/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Actividad4
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        //Gestiona el evento 
+
+        private void btnMsg_Click(object sender, RoutedEventArgs e)
+        {
+            // Navega hacia un pagina tipo PaginaMsg con el parametro ingresado en el TextBox
+            Frame.Navigate(typeof(PaginaMsg),txtMgs.Text);
+        }
+    }
+}
